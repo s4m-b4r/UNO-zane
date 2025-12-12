cc13b4b4706c75eab53d5911633bbd8dec2dc640
Some-Pigeon0 <zainomer987@outlook.com>
2025-12-12 14:22:57 +0000

    z

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ io.on('connection', (socket) => {
 
   socket.on("colour change", (data) => {
     console.log("trying to change the colour")
-    games[Number(data.room)].discardPile[discardPile.length - 1][1] += data.colourChanged
+    games[Number(data.room)].discardPile[games[Number(data.room)].discardPile.length - 1][1] += data.colourChanged
     games[Number(data.room)].ChangeColourMode = false
     io.to(String(data.room)).emit("gameUpdate", { discardPile1: games[Number(data.room)].discardPile, ChangeColourMode: games[Number(data.room)].ChangeColourMode })
   })
