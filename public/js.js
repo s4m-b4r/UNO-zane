d80df98cf4cc316c95b223c580b4fb335206fbd0
Some-Pigeon0 <zainomer987@outlook.com>
2026-01-14 22:34:43 +0000

    z

--- public/js.js
+++ public/js.js
@@ -91,7 +91,7 @@ function draw() {
         push()
         fill("black")
         textSize(40)
-        text(maxplayer, width/2 +220, height/4 + 10)
+        text(maxplayer, width/2 +220, height/4 + 15)
         pop()
         break
         
